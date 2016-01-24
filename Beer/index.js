6bad03a5f3e5ad0ff803d6daa63380b071b2ad27
Benjamin <benjamin@tracz.fr>
2016-01-24 15:36:51 +0100

    ajout énoncés

--- Beer/index.js
+++ Beer/index.js
@@ -1,6 +1,30 @@
 /**
  * Created by Benjamin on 24/01/2016.
  */
+//       =====================================
+//           Exercice 6 – La bière des architectes
+//       =====================================
+//
+//           Un architecte finit toujours sa bière.
+//           La réserve de bières du pub contient X litres de bière.
+//       - L'architecte commence à les boires en girafes de 3L ;
+//       - Puis en pinte de 0,5L ;
+//       - Puis en demi de 0,25L ;
+//       - Enfin en galopin de 0,125L ;
+//       Sachant que chaque verre doit être vide avant d'en commander un autre.
+//
+//       Combien de consommation de chaque type prendra-t-il ?
+//
+//           En entrée:
+//       - Le nombre de litres dans la réserve du pub au début de la journée de travail.
+//
+//           Sortie
+//       - Un chaine indiquant le nombre de verre consommés de la forme:
+//           L'architecte va boire x girafe(s), y pinte(s), z demi(s) et w galopin(s) avant d'aller faire un autre bar...
+//
+//           Donner ces informations pour :
+//           - 1000,23 Litres
+//       - 123,456 Litres
 var beer = require('../Beer/beer.js');
 
 var l = 1000.23;
--- CompteEstBon/index.js
+++ CompteEstBon/index.js
@@ -1,6 +1,18 @@
 /**
  * Created by Benjamin on 24/01/2016.
  */
+//        ===============================
+//            Exercice 7 – Chiffres & Lettres
+//        ===============================
+//
+//            Tout le monde connait l'(in)démodable jeu des chiffres et des lettres et son éternel « le compte est bon ».
+//        Le but de l'algorithme est, à partir d'une liste de nombres entiers et d'un résultat entier, de déterminer l'ensemble des opérations mathématiques (+, -, * et / entière) permettant de transformer ces nombres en résultat.
+//
+//            En option, si aucun résultat exact n'existe (ou n'est trouvé), l'algorithme pourra proposer un résultat approchant.
+//
+//        Exemple :
+//            100, 10, 5 pour faire 115
+
 var compte = require('../CompteEstBon/compte.js');
 
 var plaques = [100, 15, 25, 2, 3, 7];
