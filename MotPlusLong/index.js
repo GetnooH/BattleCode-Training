442507609c2ce9d50ed6bfac9fed4f7ebe40e8c8
Benjamin <benjamin@tracz.fr>
2016-01-24 16:43:03 +0100

    Le mot le plus long

---
+++ MotPlusLong/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Benjamin on 24/01/2016.
+ */
+
+var words = require('../MotPlusLong/words.js');
+
+var draw = "aetdurejlm";
+words.findLongestWord(draw);
+draw = "etecao";
+words.findLongestWord(draw);
+draw = "smarinousz";
+words.findLongestWord(draw);
+draw = "npticaz";
+words.findLongestWord(draw);
+draw = "hkhkhkhkhk";
+words.findLongestWord(draw);
+
